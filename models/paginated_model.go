9509a10488971042eef4b9d92eb1b12bf5dc81ee
sedind <sedin.dugum@gmail.com>
2020-04-27 15:40:53 +0200

    style(*): reordered models to domain package

--- models/paginated_model.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/go-flow/template-api/pkg/paging"
-
-// PaginatedModel model
-type PaginatedModel struct {
-	Results   interface{}       `json:"results"`
-	Paginator *paging.Paginator `json:"paginator"`
-}
